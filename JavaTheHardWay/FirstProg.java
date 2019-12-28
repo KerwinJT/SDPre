c6712c63ab6d5c881aaedc1586ccdedb48ca0f47
KerwinJT <kerwinjt@gmail.com>
2019-12-28 14:34:22 -0700

    Added comments to lines

--- JavaTheHardWay/FirstProg.java
+++ JavaTheHardWay/FirstProg.java
@@ -1,6 +1,6 @@
 public class FirstProg {
   public static void main (String[] args) {
-    System.out.println("I am determined to learn how to code.");
-    System.out.println( "Today's Date is 12/28/2019");
+    System.out.println("I am determined to learn how to code."); //Prints string then adds line
+    System.out.println( "Today's Date is 12/28/2019"); //Prints string then adds line
   }
 }
