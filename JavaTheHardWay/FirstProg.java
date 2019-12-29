60a6d22191afa31ba2d1b6163221651376375006
KerwinJT <kerwinjt@gmail.com>
2019-12-28 18:01:51 -0700

    Added Study Drills

--- JavaTheHardWay/FirstProg.java
+++ JavaTheHardWay/FirstProg.java
@@ -1,6 +1,6 @@
 public class FirstProg {
   public static void main (String[] args) {
-    System.out.println("I am determined to learn how to code."); //Prints string then adds line
-    System.out.println( "Today's Date is 12/28/2019"); //Prints string then adds line
+    System.out.println("I, Jason, am determined to learn how to code."); //Prints string then adds line
+    System.out.println("Today's Date is 12/28/2019"); //Prints string then adds line
   }
 }
