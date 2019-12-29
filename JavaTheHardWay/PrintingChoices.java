b4f35463e5ada99c442c095c048dfe8628495060
KerwinJT <kerwinjt@gmail.com>
2019-12-29 09:19:57 -0700

    Exercise 3 with study drills

---
+++ JavaTheHardWay/PrintingChoices.java
@@ -0,0 +1,24 @@
+public class PrintingChoices{
+  public static void main ( String [] args){
+    System.out.println("Aplha");
+    System.out.println("Beta");
+    //This space will not be printed
+    System.out.println("Charlie");
+    System.out.println("Delta");
+    System.out.println(); //Creates a blank line
+
+    System.out.print("Echo"); //These next two print methods will print on
+    System.out.print("Foxtrot"); //on the same line.
+
+    System.out.println("Golf");
+    System.out.print("Hotel");
+    System.out.println();//Creates a blank line
+    System.out.println("India");
+
+    System.out.println();
+    System.out.println("This" + " " + "is" + " " + "a" + " test.");
+
+    System.out.println("I am " + "learning Java " + "the hard way!!!"); //This is the study drill
+
+  }
+}
