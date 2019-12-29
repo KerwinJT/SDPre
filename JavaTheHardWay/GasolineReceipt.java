b1986ec4d64c54dcb42ff9405669d12f36571a7d
KerwinJT <kerwinjt@gmail.com>
2019-12-29 08:57:53 -0700

    Updated for study drills

--- JavaTheHardWay/GasolineReceipt.java
+++ JavaTheHardWay/GasolineReceipt.java
@@ -4,12 +4,12 @@ public class GasolineReceipt{
     System.out.println("|                        |" );
     System.out.println("|      CORNER STORE      |" );
     System.out.println("|                        |" );
-    System.out.println("| 2015-03-29  04:38PM    |" );
+    System.out.println("| 2019-12-29  05:00PM    |" );
     System.out.println("|                        |" );
-    System.out.println("| Gallons:       10.870  |" );
-    System.out.println("| Price/gallon: $ 2.089  |" );
+    System.out.println("| Gallons:       16.630  |" );
+    System.out.println("| Price/gallon: $ 3.099  |" );
     System.out.println("|                        |" );
-    System.out.println("| Fuel total:  $ 22.71   |" );
+    System.out.println("| Fuel total:  $ 51.54   |" );
     System.out.println("|                        |" );
     System.out.println("+------------------------+" );
   }
