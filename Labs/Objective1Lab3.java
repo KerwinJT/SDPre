b56283895c1bd522bfbd01256ef243c2e1783b31
KerwinJT <kerwinjt@gmail.com>
2019-12-30 14:58:09 -0700

    Lab3 solution

---
+++ Labs/Objective1Lab3.java
@@ -0,0 +1,7 @@
+public class Objective1Lab3{
+  public static void main (String [] args){
+    System.out.println("Name : Jason K.");
+    System.out.println("Age : 33");
+    System.out.println("State : Colorado");
+  }
+}
