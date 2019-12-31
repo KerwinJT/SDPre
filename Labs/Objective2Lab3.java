2f20a5b04cc8c8af5184efee531fd3a8f6f5d82e
KerwinJT <kerwinjt@gmail.com>
2019-12-30 20:31:15 -0700

    Objective 3 Lab3 solutions.

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,11 @@
+public class Objective2Lab3{
+  public static void main (String [] args){
+    String food1, food2, food3;
+
+    food1 = "Tiramisu";
+    food2 = "Pizza";
+    food3 = "Chicken Makhani";
+
+    System.out.println("My favorite foods are: " + food1 + ", " + food2 + ", and " + food3 + ".");
+  }
+}
