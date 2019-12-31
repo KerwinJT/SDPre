b3ff24503948fa830b4cb18ddd153a1ec80efd9f
KerwinJT <kerwinjt@gmail.com>
2019-12-30 20:23:45 -0700

    Objective 2 Lab 2 solution

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,10 @@
+public class Objective2Lab2 {
+  public static void main (String [] args){
+    String name = "Jason K.";
+    int age = 33;
+    String state = "Colorado";
+
+    System.out.println("My name is " + name + ". My age is " + age + ". I live in Colorado.");
+    
+  }
+}
