de580b1579d2e72c87c55e3324186385d7107271
KerwinJT <kerwinjt@gmail.com>
2019-12-31 14:21:47 -0700

    solution for Objective 2 Lab 4

---
+++ Labs/Objective2Lab4.java
@@ -0,0 +1,14 @@
+public class Objective2Lab4 {
+  public static void main (String [] args){
+    double num1, num2, num3, average;
+
+    num1 = 6;
+    num2 = 18;
+    num3 = 35;
+
+    average = (num1 + num2 + num3) / 3;
+
+    System.out.println("The average of " + num1 + ", " + num2 + ", and " + num3 + " is " + average);
+
+  }
+}
