06c95a3a5f1b140eef21d9572f8a37d384da369f
KerwinJT <kerwinjt@gmail.com>
2019-12-31 13:39:54 -0700

    Formatted Solution

--- Labs/Objective2Lab3.java
+++ Labs/Objective2Lab3.java
@@ -1,11 +1,14 @@
-public class Objective2Lab3{
-  public static void main (String [] args){
+public class Objective2Lab3 {
+  public static void main(String [] args){
     String food1, food2, food3;
 
     food1 = "Tiramisu";
     food2 = "Pizza";
     food3 = "Chicken Makhani";
 
-    System.out.println("My favorite foods are: " + food1 + ", " + food2 + ", and " + food3 + ".");
+    System.out.println("My favorite foods are:");
+    System.out.println("1. " + food1);
+    System.out.println("2. " + food2);
+    System.out.println("3. " + food3);
   }
 }
